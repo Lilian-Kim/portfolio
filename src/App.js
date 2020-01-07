8e10a510825454fe24cbee0098e6dab6f7f9ec05
Lilian-Kim <lilian.k3104@gmail.com>
2020-01-07 21:25:39 +0900

    eslint test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.scss';
-import MemoTemplate from './components/MemoTemplate';
+import React from 'react'
+import './App.scss'
+import MemoTemplate from './components/MemoTemplate'
 
 function App() {
   return (
     <div className="App">
       <MemoTemplate>MemoTemplate</MemoTemplate>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MemoList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MemoList = () => {
+  return <div></div>
+}
+
+export default MemoList
