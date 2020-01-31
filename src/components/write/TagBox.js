4a811244bf75106d9be7b0125977d2b08548e61d
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-01 02:08:33 +0900

    tag 관련 hooks

--- src/components/write/TagBox.js
+++ src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState, useCallback} from 'react'
 import styled from 'styled-components'
 import 'styles/components/Tagbox.scss'
 
@@ -21,27 +21,62 @@ margin-top: 0.5rem;
 `
 
 //React.memo 써서 태그 값 바뀔 때만 리렌더링
-const TagItem = React.memo(({tag})=> <Tag>#{tag}</Tag>)
+const TagItem = React.memo(({tag, onRemove}) => ( 
+<Tag onClick = {()=> onRemove(tag)}>#{tag}</Tag>
+))
 
 //React.memo 써서 tag 값 바뀔 때만 리렌더링
-const TagList = React.memo(({tags})=> (
+const TagList = React.memo(({tags, onRemove})=> (
 <TagListWrapper>
     {tags.map(tag => (
-        <TagItem key={tag} tag={tag} />
+        <TagItem key={tag} tag={tag} onRemove={onRemove}/>
     ))}
 </TagListWrapper>
 ))
 
 
 const TagBox = () => {
+    const [input, setInput] = useState('')
+    const [localTags, setLocalTags] = useState([])
+
+    const insertTag = useCallback(
+        tag => {
+            if (!tag) return
+            if (localTags.includes(tag)) return
+            setLocalTags([...localTags, tag])
+        },
+        [localTags],
+    )
+    
+    const onRemove = useCallback(
+        tag => {
+            setLocalTags(localTags.filter(t => t !== tag))
+        },
+        [localTags]
+    )
+
+    const onChange = useCallback(
+        e => {
+            setInput(e.target.value)
+        }, [])
+
+    const onSubmit = useCallback(
+        e => {
+            e.preventDefault()
+            insertTag(input.trim())
+            setInput('')
+        },
+        [input, insertTag]
+    )
+
     return(
         <div className="tagbox-wrapper">
             <h4>태그</h4>
-            <form>
-                <input type="text" placeholder="태그를 입력하세요."/>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="태그를 입력하세요." value={input} onChange={onChange}/>
                 <button type="submit">추가</button>
             </form>
-            <TagList tags={['태그1', '태그2', '태그3']} />
+            <TagList tags={localTags} onRemove={onRemove} />
         </div>
     )
 }
