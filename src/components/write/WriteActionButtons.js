5dc0c0e4d8f1226cb30dd77d924123767880eff6
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-01 02:59:02 +0900

    write 리듀서

---
+++ src/components/write/WriteActionButtons.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import Button from 'components/common/Button'
+
+const WriteActionButtonWrapper = styled.div`
+width:100%;
+text-align:center;
+margin-top: 1rem;
+margin-bottom: 3rem;
+button + button {
+    margin-left: 0.5rem
+}
+`
+
+const StyledButton = styled(Button)`
+height: 2.215rem;
+font-size:1rem;
+& + &{
+    margin-left: 0.5rem;
+}
+`
+
+const WriteActionButtons = ({onCancel, onPublish}) => {
+    return(
+        <WriteActionButtonWrapper>
+            <StyledButton cyan onClick={onPublish}>
+                포스트 등록
+            </StyledButton>
+            <StyledButton onCancel={onCancel}>취소</StyledButton>
+        </WriteActionButtonWrapper>
+
+
+    )
+}
+
+export default WriteActionButtons
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Editor from 'components/write/Editor'
 import TagBox from '../components/write/TagBox'
+import WriteActionButtons from '../components/write/WriteActionButtons'
 
 const WritePage = () => {
     return(
     <>
     <Editor/>
     <TagBox />
+    <WriteActionButtons />
     </>
     )
 }
