62d79bbbddf481cc9afa1b0fc4949d1c4b801efc
Lilian-Kim <lilian.k3104@gmail.com>
2020-02-08 15:09:39 +0900

    proxy 설정 추가

---
+++ src/setProxy.js
@@ -0,0 +1,10 @@
+const proxy = require('http-proxy-middleware');
+​
+module.exports = function(app) {
+  app.use(
+    proxy('/api', {
+      target: 'http://localhost:4000',
+      changeOrigin: true
+    })
+  );
+}
